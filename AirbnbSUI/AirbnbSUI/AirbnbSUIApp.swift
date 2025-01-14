7c213624bb0b14a5053f622acdb1090665bf1b86
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-01-14 12:42:09 -0300

    Add initial project

---
+++ AirbnbSUI/AirbnbSUI/AirbnbSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbSUIApp.swift
+//  AirbnbSUI
+//
+//  Created by Dmitry Volkov on 14/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbnbSUI/AirbnbSUI/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  AirbnbSUI
+//
+//  Created by Dmitry Volkov on 14/01/2025.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
